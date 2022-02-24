990952d2872e1561a592c80fee1ab20528d62008
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-02-23 17:53:38 -0800

    added rower data and form

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,10 +14,109 @@ const [formData, setFormData] = useState({
     "nationalTeamResults": "",
     "facts": ""
   })
+
+  function handleChange(e){
+      const name = e.target.name
+      const value = e.target.value
+
+      setFormData({
+        ...formData,
+        [name]: value 
+      })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    onAddRower(formData)
+
+
+    fetch("http://localhost:3000/rowers",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+          "name": formData.name,
+          "hometown": formData.hometown,
+          "college": formData.college,
+          "image": formData.image,
+          "employer": formData.employer,
+          "collegeResults": formData.collegeResults,
+          "nationalTeamResults": formData.nationalTeamResults,
+          "facts": formData.facts
+      }),
+    })
+    .then((r) => r.json())
+    .then((newRower) => onAddRower(newRower));
+
+
+
+  }
+
     return(
-    <>
-        <h1>Form</h1>
-    </>
+    <div className = "form">
+        <h1>New Athlete Form</h1>
+        <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            name = "name"
+            placeholder="Name..."
+            onChange={handleChange}
+            value={formData.name}
+        />
+        <br/>
+        <input
+            type="text"
+            name = "hometown"
+            placeholder="Hometown..."
+            onChange={handleChange}
+            value={formData.hometown}
+        />
+        <br/>
+        <input
+            type="text"
+            name = "college"
+            placeholder="College..."
+            onChange={handleChange}
+            value={formData.college}
+        />
+        <br/>
+        <input
+            type="text"
+            name = "image"
+            placeholder="Image URL..."
+            onChange={handleChange}
+            value={formData.image}
+        />
+        <br/>
+        <input
+            type="text"
+            name = "employer"
+            placeholder="Employer(s) and Job Title..."
+            onChange={handleChange}
+            value={formData.employer}
+        />
+        <br/>
+        <input
+            type="text"
+            name = "collegeResults"
+            placeholder="Collegiate Results..."
+            onChange={handleChange}
+            value={formData.collegeResults}
+        />
+        <br/>
+        <input
+            type="text"
+            name = "nationalTeamResults"
+            placeholder="National Team Results..."
+            onChange={handleChange}
+            value={formData.nationalTeamResults}
+        />
+        <br/>
+        <button type="submit">Add Rower</button>
+        </form>
+    </div>
     )
     
 
